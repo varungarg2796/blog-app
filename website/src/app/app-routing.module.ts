26948a7ab7f0f6008ba79e08e77544aa0c5b863b
varungarg2796 <varungarg2796@hotmail.com>
2020-05-13 23:39:29 +0100

    blog component linked to navbar

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'blog',
+    component: BlogComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
